f8d701bdd7d810b5d396d6f75b37ad2bc7d100a4
ASIFMCjr <ebogdashka03112005@mail.ru>
2022-12-18 14:18:22 +0300

    YEAAAH, VICTORY!

--- src/nav/AddTodo.js
+++ src/nav/AddTodo.js
@@ -1,19 +1,38 @@
 import React,{useState} from 'react'
-import {View, StyleSheet,TouchableOpacity, Button, Image, Alert, TextInput} from 'react-native'
+import {View, StyleSheet,TouchableOpacity, Text, Button, Modal, Image, Alert, TextInput} from 'react-native'
 
 export const AddTodo = ({ onSubmit }) => {
     const [value,setValue] = useState('')
-
+    const [valueTwo,setValueTwo] = useState('')
+    const [valueThree,setValueThree] = useState('')
+    const [modalWindow, setModalWindow] = useState(false)
     const pressHandler = () => {
-        onSubmit(value)
+        onSubmit(value,valueTwo,valueThree)
         setValue('')
+        setValueTwo('')
+        setValueThree('')
+        
     }
 
     return (
         <View style={styles.block}>
-            <TouchableOpacity onPress={pressHandler}>
+            <TouchableOpacity onPress={() => {setModalWindow(true)}}>
                 <Image source={require('../../assets/addPost.png')} ></Image>
             </TouchableOpacity>
+            <Modal visible={modalWindow}>
+              <View>
+                  <TouchableOpacity onPress={() => setModalWindow(false)}>
+                      <Image source={require('../../assets/addPost.png')}></Image>
+                  </TouchableOpacity>
+                  <Text>Описание мероприятия</Text>
+                  <TextInput style={styles.modalInput} onChangeText={setValue} value={value}></TextInput>
+                  <Text>Время проведения</Text>
+                  <TextInput style={styles.modalInput} onChangeText={setValueTwo} value={valueTwo}></TextInput>
+                  <Text>Ссылки</Text>
+                  <TextInput style={styles.modalInput} onChangeText={setValueThree} value={valueThree}></TextInput>
+                  <Button title='save' onPress={() => {setModalWindow(false);pressHandler()}}>Сохранить</Button>
+              </View>
+          </Modal>
         </View>
     )
 }
@@ -30,5 +49,47 @@ const styles = StyleSheet.create({
         borderStyle: 'solid',
         borderBottomWidth: 2,
         borderBottomColor: '#3949ab'
-    }
+    },
+    container: {
+        marginTop: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign:'left',
+      },
+       img:{
+        marginLeft: 'auto',
+        marginRight: 30,
+        marginTop: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+       },
+    
+      text: {
+        fontWeight: 'normal',
+        fontSize: 30,
+        color: '#4B4B4B',
+        textAlign:'left',
+      },
+      button: {
+        padding: 10,
+        width: 300,
+        height: 80,
+        borderColor:'#989898',
+        borderWidth:1.5,
+        borderRadius: 17,
+        boxShadow:40,
+        marginTop: 25,
+        color:'#999898',
+      },
+      modalInput: {
+        marginVertical: 10,
+        paddingHorizontal: 10,
+        borderColor:'#989898',
+        borderWidth:1.5,
+        borderRadius: 17,
+      },
+      textButton: {
+        color: '#000000',
+        fontSize: 15,
+      },
 })
